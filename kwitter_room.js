8eb68aa2235f3c3fb7e2b52310e5caeda7c891b3
SriBYJU <95580233+SriBYJU@users.noreply.github.com>
2022-04-05 20:45:02 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,33 @@
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+      apiKey: "AIzaSyB4B-0kQ5Br-HKIPijAU5IXAzZBFb5IV5w",
+    
+      authDomain: "kwitter-website-3946e.firebaseapp.com",
+    
+      databaseURL: "https://kwitter-website-3946e-default-rtdb.firebaseio.com",
+    
+      projectId: "kwitter-website-3946e",
+    
+      storageBucket: "kwitter-website-3946e.appspot.com",
+    
+      messagingSenderId: "735482580007",
+    
+      appId: "1:735482580007:web:971abf67cdf7d011e29202"
+    
+    };
+    
+    
+    // Initialize Firebase
+    
+    const app = initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
